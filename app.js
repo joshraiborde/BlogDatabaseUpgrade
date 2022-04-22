72021a4deef8d5647237766bdb4d8d7c0c6a2750
joshraiborde <joshraiborde@gmail.com>
2022-04-22 17:23:51 -0400

    in app.post(/post)...changed the route parameter, created a const to store postId, added Post.findOne() and rendered title and content

--- app.js
+++ app.js
@@ -23,27 +23,75 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // create a new database
-mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
 
 // create a new schema called postSchema
 const postSchema = {
   title: String,
   content: String
 };
+
 // create a new mongoose based on the postSchema
 const Post = mongoose.model("Post", postSchema);
 
 // Delete the existing posts array.
 // let posts = [];
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res){
 // Find all the posts in the posts collection and render that in the home.ejs file.
-  Post.find({}, function(err, posts){
+Post.find({}, function(err, posts){
   res.render("home", {
     startingContent: homeStartingContent,
     posts: posts
+    });
+});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+
+app.post("/compose", function(req, res){
+  // create a new post document using the mongoose model.
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+  // save the document to the database instead of pushing to the posts array.
+  // posts.push(post);
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
   });
 });
+
+// changed the express route parameter to postId instead postName
+app.get("/posts/:postId", function(req, res){
+  // const requestedTitle = _.lowerCase(req.params.postName);
+
+  // create a const to store the postId parameter value
+  // const requestedPostId = _.lowerCase(req.params.postId);
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+  // posts.forEach(function (post) {
+  //   const storedTitle = _.lowerCase(post.title);
+
+  //   if (storedTitle === requestedTitle) {
+  //     res.render("post", {
+  //       title: post.title,
+  //       content: post.content
+  //     });
+  //   }
+  // });
 });
 
 app.get("/about", function (req, res) {
@@ -52,42 +100,6 @@ app.get("/about", function (req, res) {
 
 app.get("/contact", function (req, res) {
   res.render("contact", { contactContent: contactContent });
-});
-
-app.get("/compose", function (req, res) {
-  res.render("compose");
-});
-
-app.post("/compose", function (req, res) {
-  // create a new post document using the mongoose model.
-  const post = new Post ({
-    title: req.body.postTitle,
-    content: req.body.postBody
-  });
-  // save the document to the database instead of pushing to the posts array.
-  // posts.push(post);
-  post.save(function(err){
-    if(!err){
-
-  res.redirect("/");
-    }
-  });
-
-});
-
-app.get("/posts/:postName", function (req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
-
-  posts.forEach(function (post) {
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    }
-  });
 });
 
 app.listen(port, function () {
